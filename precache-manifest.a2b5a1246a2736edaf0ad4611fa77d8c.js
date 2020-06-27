fbb01b8f06e06d808c6e178a0da3c59813705b9a
diegoXD93 <bolivaristadiego@gmail.com>
2020-06-27 01:58:50 -0400

    deploy

---
+++ precache-manifest.a2b5a1246a2736edaf0ad4611fa77d8c.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c1dc42538b50d6a72a",
+    "url": "/frontend-mentor/css/app.ba5df3b0.css"
+  },
+  {
+    "revision": "52c94b392a8335639206f1757db3f39a",
+    "url": "/frontend-mentor/img/bg-curve-desktop.52c94b39.svg"
+  },
+  {
+    "revision": "e5592c289336112e2e3d55916eb6a118",
+    "url": "/frontend-mentor/img/bg-curve-mobile.e5592c28.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/frontend-mentor/img/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/frontend-mentor/img/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/frontend-mentor/img/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "c23dd1d0b7e4b4a1637d50cbb9fff810",
+    "url": "/frontend-mentor/img/bg-intro-mobile.c23dd1d0.svg"
+  },
+  {
+    "revision": "e9fdc10442430eaa6f39fde1ece21ac0",
+    "url": "/frontend-mentor/img/icon-arrow.e9fdc104.svg"
+  },
+  {
+    "revision": "f0f5b24aa6e8f6c57312f24da96ebf6d",
+    "url": "/frontend-mentor/img/icon-email.f0f5b24a.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/frontend-mentor/img/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "a51e688006d6bdc00db7e8fb4f923081",
+    "url": "/frontend-mentor/img/icon-quotes.a51e6880.svg"
+  },
+  {
+    "revision": "b54a697161d657d5e1a154b7e589fb65",
+    "url": "/frontend-mentor/img/illustration-1.b54a6971.svg"
+  },
+  {
+    "revision": "13d8305b15e4b62000c2e2c756b5e70c",
+    "url": "/frontend-mentor/img/illustration-2.13d8305b.svg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/frontend-mentor/img/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "ac83810fe8fa956547bbaee901ca5958",
+    "url": "/frontend-mentor/img/logo-dark.ac83810f.svg"
+  },
+  {
+    "revision": "0786a6202db9d6278c6b043c53bcc3ad",
+    "url": "/frontend-mentor/img/logo.0786a620.svg"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/frontend-mentor/img/reto-b/icon-down.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/frontend-mentor/img/reto-b/icon-facebook.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/frontend-mentor/img/reto-b/icon-instagram.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/frontend-mentor/img/reto-b/icon-twitter.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/frontend-mentor/img/reto-b/icon-up.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/frontend-mentor/img/reto-b/icon-youtube.svg"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/frontend-mentor/img/reto-c/account.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/frontend-mentor/img/reto-c/eyecam-co.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/frontend-mentor/img/reto-c/faceit.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/frontend-mentor/img/reto-c/icon-remove.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/frontend-mentor/img/reto-c/insure.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/frontend-mentor/img/reto-c/loop-studios.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/frontend-mentor/img/reto-c/manage.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/frontend-mentor/img/reto-c/myhome.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/frontend-mentor/img/reto-c/photosnap.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/frontend-mentor/img/reto-c/shortly.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/frontend-mentor/img/reto-c/the-air-filter-company.svg"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/frontend-mentor/img/reto-d/icon-api.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/frontend-mentor/img/reto-d/icon-budgeting.svg"
+  },
+  {
+    "revision": "b098a76cd2558ce72a7b730de0d57503",
+    "url": "/frontend-mentor/img/reto-d/icon-close.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/frontend-mentor/img/reto-d/icon-facebook.svg"
+  },
+  {
+    "revision": "72bac04e38489fa2115bb7c45775578f",
+    "url": "/frontend-mentor/img/reto-d/icon-hamburger.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/frontend-mentor/img/reto-d/icon-instagram.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/frontend-mentor/img/reto-d/icon-onboarding.svg"
+  },
+  {
+    "revision": "7a9111dbfa3b4b2ea0d088435bfe1d76",
+    "url": "/frontend-mentor/img/reto-d/icon-online.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/frontend-mentor/img/reto-d/icon-pinterest.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/frontend-mentor/img/reto-d/icon-twitter.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/frontend-mentor/img/reto-d/icon-youtube.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/frontend-mentor/img/reto-d/image-confetti.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/frontend-mentor/img/reto-d/image-currency.jpg"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/frontend-mentor/img/reto-d/image-plane.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/frontend-mentor/img/reto-d/image-restaurant.jpg"
+  },
+  {
+    "revision": "9c0f63d581cf6b4f58941ad935911c95",
+    "url": "/frontend-mentor/index.html"
+  },
+  {
+    "revision": "82c1dc42538b50d6a72a",
+    "url": "/frontend-mentor/js/app.f2c5e0dd.js"
+  },
+  {
+    "revision": "e5183da4c13e48a5fb12",
+    "url": "/frontend-mentor/js/chunk-vendors.b49c8735.js"
+  },
+  {
+    "revision": "790bb0ce1753d32f05867375b9937f82",
+    "url": "/frontend-mentor/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/frontend-mentor/robots.txt"
+  }
+]);
